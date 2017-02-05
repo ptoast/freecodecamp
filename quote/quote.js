80a5f7e9df3140fd503f080b6b32eff8fe43864f
ptoast <ptoast4@gmail.com>
2017-02-05 13:27:59 -0500

    simplified tweeting process

--- quote/quote.js
+++ quote/quote.js
@@ -18,23 +18,5 @@ $(document).ready(function(){
 function parseQuote(data){
   $('#quote').html(data.quoteText);
   $('#author').html(data.quoteAuthor);
-  $('#tweet').attr('href', 'https://twitter.com/intent/tweet' + '?text=' + data.quoteText + ' --' + data.quoteAuthor);
-
-  window.twttr = (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0],
-      t = window.twttr || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
-
-    t._e = [];
-    t.ready = function(f) {
-      t._e.push(f);
-    };
-
-    return t;
-  }(document, "script", "twitter-wjs"));
-
+  $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + data.quoteText + '" -' + data.quoteAuthor));
 }
