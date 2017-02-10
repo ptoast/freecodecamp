030fa2a06e9a3cca973fd4ba03dc6d88a657bc7a
Jack <ptoast4@gmail.com>
2017-02-09 19:53:53 -0500

    working location and C-F conversion

--- weather/weather.js
+++ weather/weather.js
@@ -1,3 +1,7 @@
+var temp = 0;
+var metric = false;
+var loc = '';
+
 $(document).ready(function(){
 
   $('#s_weather').click(function(e) {
@@ -8,10 +12,52 @@ $(document).ready(function(){
       dataType: 'jsonp'
     });
   });
+
+  $('#t_units').click(function(e) {
+    e.preventDefault();
+    metric = !metric;
+    if (metric) {
+      $('#t_units').html("°C");
+    } else {
+      $('#t_units').html("°F");
+    }
+    $('#temperature').html(getTemp());
+  });
+
+  $('#my_location').click(function(e) {
+    e.preventDefault();
+    getLocation();
+  });
 })
 
 function parseWeather(data){
-  $('#location').html('Location >> ' + data.name);
-  $('#temperature').html('Temp >> ' + data.main.temp + "&degF");
-  $('#condition').html('Conditions >> ' + data.weather[0].description);
+  temp = data.main.temp;
+  loc = data.name;
+  $('#location').html(loc);
+  $('#temperature').html(getTemp());
+  $('#condition').html(data.weather[0].description);
+}
+
+function getTemp(){
+  if (metric) {
+    return Math.round((temp - 32) / 1.8) + "°C";
+  } else {
+    return Math.round(temp) + "°F";
+  }
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(usePosition);
+    }
+}
+
+function usePosition(position) {
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
+
+  $.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&APPID=ad75be96f1a700e43f904674b3eaa49d&callback=parseWeather',
+    dataType: 'jsonp'
+  });
 }
